273859c2748770fe1d88756a442de9c083041fc1
James <89285665+JimboDaDuck@users.noreply.github.com>
2022-09-13 18:52:02 +1000

    Add files via upload

---
+++ browser.py
@@ -0,0 +1,65 @@
+import sys
+import ctypes
+from PyQt5.QtCore import *
+from PyQt5.QtWidgets import *
+from PyQt5.QtWebEngineWidgets import *
+from PyQt5 import QtGui
+from PyQt5.QtGui import QFont
+
+
+
+class MainWindow(QMainWindow):
+    def __init__(self):
+        super(MainWindow, self).__init__()
+        self.setWindowIcon(QtGui.QIcon('content/logo.png'))
+        self.browser = QWebEngineView()
+        self.browser.setUrl(QUrl('http://google.com'))
+        self.setCentralWidget(self.browser)
+        self.showMaximized()
+
+        # navbar
+        navbar = QToolBar()
+        self.addToolBar(navbar)
+
+        back_btn = QAction('<', self)
+        back_btn.setFont(QFont('Arial', 20))
+        back_btn.triggered.connect(self.browser.back)
+        navbar.addAction(back_btn)
+
+        forward_btn = QAction('>', self)
+        forward_btn.setFont(QFont('Arial', 20))
+        forward_btn.triggered.connect(self.browser.forward)
+        navbar.addAction(forward_btn)
+
+        reload_btn = QAction('Reload', self)
+        reload_btn.setIcon(QtGui.QIcon('content/reloadico.png'))
+        reload_btn.triggered.connect(self.browser.reload)
+        navbar.addAction(reload_btn)
+
+        home_btn = QAction('Home', self)
+        home_btn.setIcon(QtGui.QIcon('content/homeico.png'))
+        home_btn.triggered.connect(self.navigate_home)
+        navbar.addAction(home_btn)
+
+        self.url_bar = QLineEdit()
+        self.url_bar.returnPressed.connect(self.navigate_to_url)
+        navbar.addWidget(self.url_bar)
+
+        self.browser.urlChanged.connect(self.update_url)
+
+    def navigate_home(self):
+        self.browser.setUrl(QUrl('https://www.google.com/'))
+
+    def navigate_to_url(self):
+        url = self.url_bar.text
+        self.browser.setUrl(QUrl(url))
+
+    def update_url(self, q):
+        self.url_bar.setText(q.toString())
+
+myappid = 'Ji9sw.JIT9.Browser.1.0'
+ctypes.windll.shell32.SetCurrentProcessExplicitAppUserModelID(myappid)
+app = QApplication(sys.argv)
+QApplication.setApplicationName('JIT9')
+window = MainWindow()
+app.exec_()
